6139569f09016735f8f10d154a44c0b28d9d7c4e
Per Liedman <per@liedman.net>
2013-11-11 13:59:41 +0100

    Fixed various show stoppers with Bing and RaveGeo geocoders

--- Control.Geocoder.js
+++ Control.Geocoder.js
@@ -1,94 +1,102 @@
-L.Control.Geocoder = L.Control.extend({
-	options: {
-		collapsed: true,
-		position: 'topright',
-		placeholder: 'Search...',
-		text: 'Locate',
-		errorMessage: 'Nothing found.'
-	},
+(function (factory) {
+	// Packaging/modules magic dance
+	var L;
+	if (typeof define === 'function' && define.amd) {
+		// AMD
+		define(['leaflet'], factory);
+	} else if (typeof module !== 'undefined') {
+		// Node/CommonJS
+		L = require('leaflet');
+		module.exports = factory(L);
+	} else {
+		// Browser globals
+		if (typeof window.L === 'undefined')
+			throw 'Leaflet must be loaded first';
+		factory(window.L);
+	}
+}(function (L) {
+	'use strict';
+	L.Control.Geocoder = L.Control.extend({
+		options: {
+			collapsed: true,
+			expand: 'click',
+			position: 'topright',
+			placeholder: 'Search...',
+			errorMessage: 'Nothing found.'
+		},
 
-	_callbackId: 0,
+		_callbackId: 0,
 
-	initialize: function (key, options) {
-		this.key = key;
-		L.Util.setOptions(this, options);
-	},
+		initialize: function (options) {
+			L.Util.setOptions(this, options);
+			if (!this.options.geocoder) {
+				this.options.geocoder = new L.Control.Geocoder.Nominatim();
+			}
+		},
 
-	onAdd: function (map) {
-		this._map = map;
-		var className = 'leaflet-control-geocoder',
-			container = this._container = L.DomUtil.create('div', className);
+		onAdd: function (map) {
+			this._map = map;
+			var className = 'leaflet-control-geocoder';
 
-		L.DomEvent.disableClickPropagation(container);
+			var form = this._form = L.DomUtil.create('form', className + '-form');
 
-		var form = this._form = L.DomUtil.create('form', className + '-form');
+			var input = this._input = document.createElement('input');
+			input.type = 'text';
 
-		var input = this._input = document.createElement('input');
-		input.type = "text";
-		input.placeholder = this.options.placeholder;
-		L.DomEvent.addListener(input, 'onkeydown', this._clearResults, this);
-		L.DomEvent.addListener(input, 'onpaste', this._clearResults, this);
-		L.DomEvent.addListener(input, 'oninput', this._clearResults, this);
+			L.DomEvent.addListener(input, 'keydown', this._keydown, this);
+			//L.DomEvent.addListener(input, 'onpaste', this._clearResults, this);
+			//L.DomEvent.addListener(input, 'oninput', this._clearResults, this);
 
-		var submit = document.createElement('button');
-		submit.type = "submit";
-		submit.innerHTML = this.options.text;
+			this._errorElement = document.createElement('div');
+			this._errorElement.className = className + '-form-no-error';
+			this._errorElement.innerHTML = this.options.errorMessage;
 
-		this._errorElement = document.createElement('div');
-		this._errorElement.className = className + "-form-no-error"
-		this._errorElement.innerHTML = this.options.errorMessage;
+			this._alts = L.DomUtil.create('ul', className + '-alternatives');
+			this._alts.style.display = 'none';
 
-		var altsTable = L.DomUtil.create('table', className + '-alternatives');
-		this._alts = document.createElement('tbody');
-		altsTable.appendChild(this._alts);
+			form.appendChild(input);
+			form.appendChild(this._errorElement);
+			form.appendChild(this._alts);
 
-		form.appendChild(input);
-		form.appendChild(submit);
-		form.appendChild(this._errorElement);
-		form.appendChild(altsTable);
+			L.DomEvent.addListener(form, 'submit', this._geocode, this);
 
-		L.DomEvent.addListener(form, 'submit', this._geocode, this);
+			if (this.options.collapsed) {
+				if (this.options.expand === 'click') {
+					L.DomEvent.addListener(input, 'click', function(e) {
+						// TODO: touch
+						if (e.button === 0 && e.detail === 1) {
+							this._toggle();
+						}
+					}, this);
+				} else {
+					L.DomEvent.addListener(input, 'mouseover', this._expand, this);
+					L.DomEvent.addListener(input, 'mouseout', this._collapse, this);
+					this._map.on('movestart', this._collapse, this);
+				}
+			} else {
+				this._expand();
+			}
 
-		if (this.options.collapsed) {
-			L.DomEvent.addListener(container, 'mouseover', this._expand, this);
-			L.DomEvent.addListener(container, 'mouseout', this._collapse, this);
+			return form;
+		},
 
-			var link = this._layersLink = L.DomUtil.create('a', className + '-toggle', container);
-			link.href = '#';
-			link.title = 'Geocoder';
+		_geocodeResult: function (results) {
+			this._form.className = this._form.className.replace(' leaflet-control-geocoder-throbber', '');
+			if (results.length === 1) {
+				this.markGeocode(results[0]);
+			} else if (results.length > 0) {
+				this._results = results;
+				this._alts.style.display = 'block';
+				for (var i = 0; i < results.length; i++) {
+					this._alts.appendChild(this._createAlt(results[i], i));
+				}
+			} else {
+				L.DomUtil.addClass(this._errorElement, 'leaflet-control-geocoder-error');
+			}
+		},
 
-			L.DomEvent.addListener(link, L.Browser.touch ? 'click' : 'focus', this._expand, this);
-
-			this._map.on('movestart', this._collapse, this);
-		} else {
-			this._expand();
-		}
-
-		container.appendChild(form);
-
-		return container;
-	},
-
-	_geocodeResult: function (results) {
-		var toggle = document.getElementsByClassName('leaflet-control-geocoder-toggle')[0];
-		toggle.className = toggle.className.replace(' leaflet-control-geocoder-throbber', '')
-		if (results.length == 1) {
-			this.markGeocode(results[0]);
-		} else if (results.length > 0) {
-			this._results = results;
-			for (var i = 0; i < results.length; i++) {
-				this._alts.appendChild(this._createAltRow(results[i]));
-			};
-		} else {
-			L.DomUtil.addClass(this._errorElement, 'leaflet-control-geocoder-error')
-		}
-	},
-
-	markGeocode: function(result) {
-			var bbox = result.bbox,
-				sw = [bbox[0], bbox[1]],
-				ne = [bbox[2], bbox[3]];
-			this._map.fitBounds([sw, ne]);
+		markGeocode: function(result) {
+			this._map.fitBounds(result.bbox);
 
 			if (this._geocodeMarker) {
 				this._map.removeLayer(this._geocodeMarker);
@@ -98,88 +106,226 @@ L.Control.Geocoder = L.Control.extend({
 				.bindPopup(result.name)
 				.addTo(this._map)
 				.openPopup();
-	},
 
-	jsonp: function(url, params, callback, context, jsonpParam) {		
-		var callbackId = "_l_geocoder_" + (this._callbackId++);
-		params[jsonpParam || "callback"] = callbackId
-		window[callbackId] = L.Util.bind(callback, context || this);
-		script = document.createElement("script");
-		script.type = "text/javascript";
+			return this;
+		},
+
+		_geocode: function(event) {
+			L.DomEvent.preventDefault(event);
+
+			this._form.className += ' leaflet-control-geocoder-throbber';
+			this._clearResults();
+			this.options.geocoder.geocode(this._input.value, this._geocodeResult, this);
+
+			return false;
+		},
+
+		_toggle: function() {
+			if (this._container.className.indexOf('leaflet-control-geocoder-expanded') >= 0) {
+				this._collapse();
+			} else {
+				this._expand();
+			}
+		},
+
+		_expand: function () {
+			L.DomUtil.addClass(this._container, 'leaflet-control-geocoder-expanded');
+		},
+
+		_collapse: function () {
+			this._container.className = this._container.className.replace(' leaflet-control-geocoder-expanded', '');
+			this._alts.style.display = 'none';
+		},
+
+		_clearResults: function () {
+			this._alts.style.display = 'none';
+			this._alts.innerHTML = '';
+			this._selection = null;
+			L.DomUtil.removeClass(this._errorElement, 'leaflet-control-geocoder-error');
+		},
+
+		_createAlt: function(result, index) {
+			var li = document.createElement('li');
+			li.innerHTML = '<a href="#" data-result-index="' + index + '">' + result.name + '</a>';
+			L.DomEvent.addListener(li, 'click', function clickHandler() {
+				this.markGeocode(result);
+			}, this);
+
+			return li;
+		},
+
+		_keydown: function(e) {
+			var _this = this,
+				select = function select(dir) {
+					if (_this._selection) {
+						L.DomUtil.removeClass(_this._selection.firstChild, 'leaflet-control-geocoder-selected');
+						_this._selection = _this._selection[dir > 0 ? 'nextSibling' : 'previousSibling'];
+					}
+					if (!_this._selection) {
+						_this._selection = _this._alts[dir > 0 ? 'firstChild' : 'lastChild'];
+					}
+
+					if (_this._selection) {
+						L.DomUtil.addClass(_this._selection.firstChild, 'leaflet-control-geocoder-selected');
+					}
+				};
+
+			switch (e.keyCode) {
+			// Up
+			case 38:
+				select(-1);
+				L.DomEvent.preventDefault(e);
+				break;
+			// Up
+			case 40:
+				select(1);
+				L.DomEvent.preventDefault(e);
+				break;
+			// Enter
+			case 13:
+				if (this._selection) {
+					var index = parseInt(this._selection.firstChild.getAttribute('data-result-index'), 10);
+					this.markGeocode(this._results[index]);
+					this._clearResults();
+					L.DomEvent.preventDefault(e);
+				}
+			}
+			return true;
+		}
+	});
+
+	L.Control.geocoder = function(id, options) {
+		return new L.Control.Geocoder(id, options);
+	};
+
+	L.Control.Geocoder.callbackId = 0;
+	L.Control.Geocoder.jsonp = function(url, params, callback, context, jsonpParam) {
+		var callbackId = '_l_geocoder_' + (L.Control.Geocoder.callbackId++);
+		params[jsonpParam || 'callback'] = callbackId;
+		window[callbackId] = L.Util.bind(callback, context);
+		var script = document.createElement('script');
+		script.type = 'text/javascript';
 		script.src = url + L.Util.getParamString(params);
 		script.id = callbackId;
-		document.getElementsByTagName("head")[0].appendChild(script);
-	},
+		document.getElementsByTagName('head')[0].appendChild(script);
+	};
 
-	_geocode: function(event) {
-		var toggle = document.getElementsByClassName('leaflet-control-geocoder-toggle')[0];
-		toggle.className += ' leaflet-control-geocoder-throbber';
-		this._clearResults();
-		L.DomEvent.preventDefault(event);
-		this.geocode(this._input.value);
-	},
+	L.Control.Geocoder.Nominatim = L.Class.extend({
+		options: {
+			serviceUrl: 'http://nominatim.openstreetmap.org/search/'
+		},
 
-	geocode: function(query) {
-		this.jsonp("http://nominatim.openstreetmap.org/search/", {
-			q: query,
-			limit: 5,
-			format: "json"
-		}, function(data) {
-			var results = [];
-			for (var i = data.length - 1; i >= 0; i--) {
-				var bbox = data[i].boundingbox;
-				for (var j = 0; j < 4; j++) bbox[j] = parseFloat(bbox[j]);
-				results[i] = {
-					name: data[i].display_name, 
-					bbox: [bbox[0], bbox[2], bbox[1], bbox[3]],
-					center: [(bbox[0] + bbox[1]) / 2, (bbox[2] + bbox[3]) / 2]
-				};
-			};
-			this._geocodeResult.call(this, results);
-		}, this, "json_callback")
-	},
+		initialize: function(options) {
+			L.Util.setOptions(this, options);
+		},
 
-	_expand: function () {
-		L.DomUtil.addClass(this._container, 'leaflet-control-geocoder-expanded');
-	},
+		geocode: function(query, cb, context) {
+			L.Control.Geocoder.jsonp(this.options.serviceUrl, {
+				q: query,
+				limit: 5,
+				format: 'json'
+			}, function(data) {
+				var results = [];
+				for (var i = data.length - 1; i >= 0; i--) {
+					var bbox = data[i].boundingbox;
+					for (var j = 0; j < 4; j++) bbox[j] = parseFloat(bbox[j]);
+					results[i] = {
+						name: data[i].display_name,
+						bbox: L.latLngBounds([bbox[0], bbox[2]], [bbox[1], bbox[3]]),
+						center: L.latLng((bbox[0] + bbox[1]) / 2, (bbox[2] + bbox[3]) / 2)
+					};
+				}
+				cb.call(context, results);
+			}, this, 'json_callback');
+		},
+	});
 
-	_collapse: function () {
-		this._container.className = this._container.className.replace(' leaflet-control-geocoder-expanded', '');
-	},
+	L.Control.Geocoder.nominatim = function(options) {
+		return new L.Control.Geocoder.Nominatim(options);
+	};
 
-	_clearResults: function () {
-		this._alts.innerHTML = "";
-		L.DomUtil.removeClass(this._errorElement, 'leaflet-control-geocoder-error');
-	},
+	L.Control.Geocoder.Bing = L.Class.extend({
+		initialize: function(key) {
+			this.key = key;
+		},
 
-	_createAltRow: function(result) {
-		var _this = this,
-			tr = document.createElement('tr');
-		tr.innerHTML = '<td><a href="#">' + result.name + '</a></td>';
-		tr.onclick = function() { 
-			_this.markGeocode.call(_this, result);
-		};
+		geocode : function (query, cb, context) {
+			L.Control.Geocoder.jsonp('http://dev.virtualearth.net/REST/v1/Locations', {
+				query: query,
+				key : this.key
+			}, function(data) {
+				var results = [];
+				for (var i = data.resourceSets[0].resources.length - 1; i >= 0; i--) {
+					var resource = data.resourceSets[0].resources[i],
+						bbox = resource.bbox;
+					results[i] = {
+						name: resource.name,
+						bbox: L.latLngBounds([bbox[0], bbox[1]], [bbox[2], bbox[3]]),
+						center: L.latLng(resource.point.coordinates)
+					};
+				}
+				cb.call(context, results);
+			}, this, 'jsonp');
+		},
+	});
 
-		return tr;
-	}
-});
+	L.Control.Geocoder.bing = function(key) {
+		return new L.Control.Geocoder.Bing(key);
+	};
 
-L.Control.Geocoder.Bing = L.Control.Geocoder.extend({
-	geocode : function (query) {
-		this.jsonp("http://dev.virtualearth.net/REST/v1/Locations", {
-			query: query,
-			key : this.key,
-		}, function(data) {
-			var results = [];
-			for (var i = data.resourceSets.resources.length - 1; i >= 0; i--) {
-				var resource = data.resourceSets.resources[i];
-				results[i] = {
-					name: resource.name, 
-					bbox: resouce.bbox,
-					center: resource.point.coordinates
-				};
-			};
-			this._geocodeResult.call(this, results);
-		}, this, 'jsonp')
-	},
-})
+	L.Control.Geocoder.RaveGeo = L.Class.extend({
+		options: {
+			querySuffix: '',
+			deepSearch: true,
+			wordBased: false
+		},
+
+		jsonp: function(params, callback, context) {
+			var callbackId = '_l_geocoder_' + (L.Control.Geocoder.callbackId++);
+			params.prepend = callbackId + '(';
+			params.append = ')';
+			window[callbackId] = L.Util.bind(callback, context);
+			var script = document.createElement('script');
+			script.type = 'text/javascript';
+			script.src = this._serviceUrl + '?' + L.Util.getParamString(params);
+			script.id = callbackId;
+			document.getElementsByTagName('head')[0].appendChild(script);
+		},
+
+		initialize: function(serviceUrl, scheme, options) {
+			L.Util.setOptions(this, options);
+
+			this._serviceUrl = serviceUrl;
+			this._scheme = scheme;
+		},
+
+		geocode: function(query, cb, context) {
+			L.Control.Geocoder.jsonp(this._serviceUrl, {
+				address: query + this.options.querySuffix,
+				scheme: this._scheme,
+				outputFormat: 'jsonp',
+				deepSearch: this.options.deepSearch,
+				wordBased: this.options.wordBased
+			}, function(data) {
+				var results = [];
+				for (var i = data.length - 1; i >= 0; i--) {
+					var r = data[i],
+						c = L.latLng(r.y, r.x);
+					results[i] = {
+						name: r.address,
+						bbox: L.latLngBounds([c]),
+						center: c
+					};
+				}
+				cb.call(context, results);
+			}, this);
+		}
+	});
+
+	L.Control.Geocoder.raveGeo = function(serviceUrl, scheme, options) {
+		return new L.Control.Geocoder.RaveGeo(serviceUrl, scheme, options);
+	};
+
+
+	return L.Control.Geocoder;
+}));
