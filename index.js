a48d154b13b4efe7a1e6ba53aa83bf12fd62d8e6
Per Liedman <per@liedman.net>
2013-11-11 14:00:13 +0100

    Made geocoder backend selectable, use latest code

---
+++ index.js
@@ -0,0 +1,42 @@
+(function() {
+    var map = L.map('map').setView([0, 0], 2),
+        geocoders = {
+            'Nominatim': L.Control.Geocoder.nominatim(),
+            'Bing': L.Control.Geocoder.bing('AoArA0sD6eBGZyt5PluxhuN7N7X1vloSEIhzaKVkBBGL37akEVbrr0wn17hoYAMy')/*,
+            'RaveGeo': L.Control.Geocoder.raveGeo('http://services.geosition.com/ravegeo3/geocode2/Geocode', 'Teleatlas_Demo')*/
+        },
+        selector = L.DomUtil.get('geocode-selector'),
+        control = new L.Control.Geocoder({ geocoder: null }),
+        btn,
+        selection;
+
+    function select(geocoder, el) {
+        if (selection) {
+            L.DomUtil.removeClass(selection, 'selected');
+        }
+
+        control.options.geocoder = geocoder;
+        L.DomUtil.addClass(el, 'selected');
+        selection = el;
+    }
+
+    for (var name in geocoders) {
+        btn = L.DomUtil.create('button', '', selector);
+        btn.innerHTML = name;
+        (function(n) {
+            L.DomEvent.addListener(btn, 'click', function() {
+                select(geocoders[n], this);
+            }, btn);
+        })(name);
+
+        if (!selection) {
+            select(geocoders[name], btn);
+        }
+    }
+
+    L.tileLayer('http://api.tiles.mapbox.com/v3/liedman.map-5qqfez0n/{z}/{x}/{y}.png', {
+        attribution: '<a href="http://osm.org/copyright">Terms & Feedback</a>'
+    }).addTo(map);
+
+    control.addTo(map);
+})();
