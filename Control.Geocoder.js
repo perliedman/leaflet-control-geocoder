a52ea21bf1d3d2432f469cff76d758638bd55a8f
Per Liedman <per@liedman.net>
2014-03-05 16:46:38 +0100

    Add L.Control.Geocoder.MapQuest

--- Control.Geocoder.js
+++ Control.Geocoder.js
@@ -388,6 +388,80 @@
 		return new L.Control.Geocoder.RaveGeo(serviceUrl, scheme, options);
 	};
 
+	L.Control.Geocoder.MapQuest = L.Class.extend({
+		initialize: function(key) {
+			// MapQuest seems to provide URI encoded API keys,
+			// so to avoid encoding them twice, we decode them here
+			this._key = decodeURIComponent(key);
+		},
+
+		_formatName: function() {
+			var r = [],
+				i;
+			for (i = 0; i < arguments.length; i++) {
+				if (arguments[i]) {
+					r.push(arguments[i]);
+				}
+			}
+
+			return r.join(', ');
+		},
+
+		geocode: function(query, cb, context) {
+			L.Control.Geocoder.jsonp('http://www.mapquestapi.com/geocoding/v1/address', {
+				key: this._key,
+				location: query,
+				limit: 5,
+				outFormat: 'json'
+			}, function(data) {
+				var results = [],
+					loc,
+					latLng;
+				if (data.results && data.results[0].locations) {
+					for (var i = data.results[0].locations.length - 1; i >= 0; i--) {
+						loc = data.results[0].locations[i];
+						latLng = L.latLng(loc.latLng);
+						results[i] = {
+							name: this._formatName(loc.street, loc.adminArea4, loc.adminArea3, loc.adminArea1),
+							bbox: L.latLngBounds(latLng, latLng),
+							center: latLng
+						};
+					}
+				}
+
+				cb.call(context, results);
+			}, this);
+		},
+
+		reverse: function(location, scale, cb, context) {
+			L.Control.Geocoder.jsonp('http://www.mapquestapi.com/geocoding/v1/reverse', {
+				key: this._key,
+				location: location.lat + ',' + location.lng,
+				outputFormat: 'json'
+			}, function(data) {
+				var results = [],
+					loc,
+					latLng;
+				if (data.results && data.results[0].locations) {
+					for (var i = data.results[0].locations.length - 1; i >= 0; i--) {
+						loc = data.results[0].locations[i];
+						latLng = L.latLng(loc.latLng);
+						results[i] = {
+							name: this._formatName(loc.street, loc.adminArea4, loc.adminArea3, loc.adminArea1),
+							bbox: L.latLngBounds(latLng, latLng),
+							center: latLng
+						};
+					}
+				}
+
+				cb.call(context, results);
+			}, this);
+		}
+	});
+
+	L.Control.Geocoder.nominatim = function(options) {
+		return new L.Control.Geocoder.Nominatim(options);
+	};
 
 	return L.Control.Geocoder;
 }));
