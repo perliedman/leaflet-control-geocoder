7e683669b7f8c17dd2ad89b10792e0f706f7306b
Justin Debbink <justin.debbink@jci.com>
2014-05-08 10:32:26 -0500

    Use protocol-relative urls when making requests

--- Control.Geocoder.js
+++ Control.Geocoder.js
@@ -232,7 +232,7 @@
 
 	L.Control.Geocoder.Nominatim = L.Class.extend({
 		options: {
-			serviceUrl: 'http://nominatim.openstreetmap.org/',
+			serviceUrl: window.location.protocol + '//nominatim.openstreetmap.org/',
 			geocodingQueryParams: {},
 			reverseQueryParams: {}
 		},
@@ -297,7 +297,7 @@
 		},
 
 		geocode : function (query, cb, context) {
-			L.Control.Geocoder.jsonp('http://dev.virtualearth.net/REST/v1/Locations', {
+			L.Control.Geocoder.jsonp(window.location.protocol + '//dev.virtualearth.net/REST/v1/Locations', {
 				query: query,
 				key : this.key
 			}, function(data) {
@@ -316,7 +316,7 @@
 		},
 
 		reverse: function(location, scale, cb, context) {
-			L.Control.Geocoder.jsonp('http://dev.virtualearth.net/REST/v1/Locations/' + location.lat + ',' + location.lng, {
+			L.Control.Geocoder.jsonp(window.location.protocol + '//dev.virtualearth.net/REST/v1/Locations/' + location.lat + ',' + location.lng, {
 				key : this.key
 			}, function(data) {
 				var results = [];
@@ -416,7 +416,7 @@
 		},
 
 		geocode: function(query, cb, context) {
-			L.Control.Geocoder.jsonp('http://www.mapquestapi.com/geocoding/v1/address', {
+			L.Control.Geocoder.jsonp(window.location.protocol + '//www.mapquestapi.com/geocoding/v1/address', {
 				key: this._key,
 				location: query,
 				limit: 5,
@@ -442,7 +442,7 @@
 		},
 
 		reverse: function(location, scale, cb, context) {
-			L.Control.Geocoder.jsonp('http://www.mapquestapi.com/geocoding/v1/reverse', {
+			L.Control.Geocoder.jsonp(window.location.protocol + '//www.mapquestapi.com/geocoding/v1/reverse', {
 				key: this._key,
 				location: location.lat + ',' + location.lng,
 				outputFormat: 'json'
