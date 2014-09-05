520cb2b5466516eb11a5c7c3c121adcf1bf99fed
Luca Corti <luca.corti@sparkling18.com>
2014-09-05 23:02:17 +0200

    Mapbox support
    Add support for Mapbox forward and reverse geocoding APIs

--- Control.Geocoder.js
+++ Control.Geocoder.js
@@ -238,6 +238,16 @@
 		script.id = callbackId;
 		document.getElementsByTagName('head')[0].appendChild(script);
 	};
+	L.Control.Geocoder.getJSON = function(url, params, callback) {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open( "GET", url + L.Util.getParamString(params), true);
+        xmlHttp.send(null);
+        xmlHttp.onreadystatechange = function () {
+            if (xmlHttp.readyState != 4) return;
+            if (xmlHttp.status != 200 && req.status != 304) return;
+            callback(JSON.parse(xmlHttp.response));
+        };
+	};
 
 	L.Control.Geocoder.Nominatim = L.Class.extend({
 		options: {
@@ -479,6 +489,83 @@
 	L.Control.Geocoder.mapQuest = function(key) {
 		return new L.Control.Geocoder.MapQuest(key);
 	};
+
+    L.Control.Geocoder.Mapbox = L.Class.extend({
+        options: {
+		    service_url: 'https://api.tiles.mapbox.com/v4/geocode/mapbox.places-v1/'
+        },
+        initialize: function(access_token) {
+            this._access_token = access_token;
+        },
+
+        geocode: function(query, cb, context) {
+            L.Control.Geocoder.getJSON(this.options.service_url + encodeURIComponent(query) + '.json', {
+                access_token: this._access_token,
+            }, function(data) {
+                var results = [],
+                    loc,
+                    latLng,
+                    latLngBounds;
+                if (data.features && data.features.length) {
+                    for (var i = 0; i <= data.features.length - 1; i++) {
+                        loc = data.features[i];
+                        latLng = L.latLng(loc.center.reverse());
+                        if(loc.hasOwnProperty('bbox'))
+                        {
+                            latLngBounds = L.latLngBounds(L.latLng(loc.bbox.slice(0, 2).reverse()), L.latLng(loc.bbox.slice(2, 4).reverse()));
+                        }
+                        else
+                        {
+                            latLngBounds = L.latLngBounds(latLng, latLng);
+                        }
+                        results[i] = {
+                            name: loc.place_name,
+                            bbox: latLngBounds,
+                            center: latLng
+                        };
+                    }
+                }
+
+                cb.call(context, results);
+            });
+        },
+
+        reverse: function(location, scale, cb, context) {
+            L.Control.Geocoder.getJSON(this.options.service_url + encodeURIComponent(location.lng) + ',' + encodeURIComponent(location.lat) + '.json', {
+                access_token: this._access_token,
+            }, function(data) {
+                var results = [],
+                    loc,
+                    latLng,
+                    latLngBounds;
+                if (data.features && data.features.length) {
+                    for (var i = 0; i <= data.features.length - 1; i++) {
+                        loc = data.features[i];
+                        latLng = L.latLng(loc.center.reverse());
+                        if(loc.hasOwnProperty('bbox'))
+                        {
+                            latLngBounds = L.latLngBounds(L.latLng(loc.bbox.slice(0, 2).reverse()), L.latLng(loc.bbox.slice(2, 4).reverse()));
+                        }
+                        else
+                        {
+                            latLngBounds = L.latLngBounds(latLng, latLng);
+                        }
+                        results[i] = {
+                            name: loc.place_name,
+                            bbox: latLngBounds,
+                            center: latLng
+                        };
+                    }
+                }
+
+                cb.call(context, results);
+            });
+        }
+    });
+
+    L.Control.Geocoder.mapbox = function(access_token) {
+        return new L.Control.Geocoder.Mapbox(access_token);
+    };
 
 	return L.Control.Geocoder;
 }));
