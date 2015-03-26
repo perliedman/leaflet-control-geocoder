4c8321267d17ac68ea3a5d4b569491f575b2b232
Per Liedman <per@liedman.net>
2015-03-26 21:31:23 +0100

    Add Photon geocoder support

--- Control.Geocoder.js
+++ Control.Geocoder.js
@@ -741,5 +741,68 @@
 	L.Control.Geocoder.google = function(key) {
 		return new L.Control.Geocoder.Google(key);
 	};
+
+	L.Control.Geocoder.Photon = L.Class.extend({
+		options: {
+			serviceUrl: '//photon.komoot.de/api/'
+		},
+
+		initialize: function(options) {
+			L.setOptions(this, options);
+		},
+
+		geocode: function(query, cb, context) {
+			var params = L.extend({
+				q: query,
+			}, this.options.geocodingQueryParams);
+
+			L.Control.Geocoder.getJSON(this.options.serviceUrl, params, function(data) {
+				var results = [],
+					i,
+					f,
+					c,
+					latLng,
+					extent,
+					bbox;
+				if (data && data.features) {
+					for (i = 0; i < data.features.length; i++) {
+						f = data.features[i];
+						c = f.geometry.coordinates;
+						latLng = L.latLng(c[1], c[0]);
+						extent = f.properties.extent;
+
+						if (extent) {
+							bbox = L.latLngBounds([extent[1], extent[0]], [extent[3], extent[2]]);
+						} else {
+							bbox = L.latLngBounds(latLng, latLng);
+						}
+
+						results.push({
+							name: f.properties.name,
+							center: latLng,
+							bbox: bbox
+						});
+					}
+				}
+
+				cb.call(context, results);
+			});
+		},
+
+		suggest: function(query, cb, context) {
+			return this.geocode(query, cb, context);
+		},
+
+		reverse: function(latLng, cb, context) {
+			// Not yet implemented in Photon
+			// https://github.com/komoot/photon/issues/19
+			cb.call(context, []);
+		}
+	});
+
+	L.Control.Geocoder.photon = function(options) {
+		return new L.Control.Geocoder.Photon(options);
+	};
+
 	return L.Control.Geocoder;
 }));
