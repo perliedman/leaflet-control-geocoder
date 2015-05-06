f521c79c2ab09f7c9671d72cc292582755866ca5
Cyrille Giquello <cyrille@giquello.fr>
2015-05-06 19:50:56 +0200

    fixes #66 - missing a length check in Geocoder.Bing

--- Control.Geocoder.js
+++ Control.Geocoder.js
@@ -415,14 +415,16 @@
 				key : this.key
 			}, function(data) {
 				var results = [];
-				for (var i = data.resourceSets[0].resources.length - 1; i >= 0; i--) {
-					var resource = data.resourceSets[0].resources[i],
-						bbox = resource.bbox;
-					results[i] = {
-						name: resource.name,
-						bbox: L.latLngBounds([bbox[0], bbox[1]], [bbox[2], bbox[3]]),
-						center: L.latLng(resource.point.coordinates)
-					};
+				if( data.resourceSets.length > 0 ){
+					for (var i = data.resourceSets[0].resources.length - 1; i >= 0; i--) {
+						var resource = data.resourceSets[0].resources[i],
+							bbox = resource.bbox;
+						results[i] = {
+							name: resource.name,
+							bbox: L.latLngBounds([bbox[0], bbox[1]], [bbox[2], bbox[3]]),
+							center: L.latLng(resource.point.coordinates)
+						};
+					}
 				}
 				cb.call(context, results);
 			}, this, 'jsonp');
