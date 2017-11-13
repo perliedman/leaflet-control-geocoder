4bcfcb6f7e0a588561700003f4fc321889954890
Per Liedman <per.liedman@dotnetmentor.se>
2017-11-13 07:56:51 +0100

    Comment out test build for now, since it's not used.

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,4 +5,4 @@ mkdir -p dist
 browserify src/index.js -t browserify-shim -t es3ify --standalone leaflet-control-geocoder | derequire >dist/Control.Geocoder.js
 cp -a Control.Geocoder.css images/ dist/
 
-browserify test/browserify.js -o test/bundle.js
+#browserify test/browserify.js -o test/bundle.js
