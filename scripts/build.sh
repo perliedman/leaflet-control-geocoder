b0799ea16831b70a76b40d64bbc74fa2d6e9ab15
Simon Legner <Simon.Legner@gmail.com>
2018-01-01 18:15:45 +0100

    Replace scripts/build.sh with an NPM script

--- scripts/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mkdir -p dist
-
-browserify src/index.js -t browserify-shim -t es3ify --standalone leaflet-control-geocoder | derequire >dist/Control.Geocoder.js
-cp -a Control.Geocoder.css images/ dist/
-
-#browserify test/browserify.js -o test/bundle.js
