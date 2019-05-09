254d63a8f7a73c475080ded99d8e8f27d7fd5ef6
Simon Legner <Simon.Legner@gmail.com>
2019-05-09 23:26:56 +0200

    Add open-location-code.spec.js

--- spec/karma.conf.js
+++ spec/karma.conf.js
@@ -4,6 +4,7 @@ module.exports = function(config) {
     plugins: ['karma-mocha', 'karma-sinon', 'karma-expect', 'karma-phantomjs-launcher'],
     frameworks: ['mocha', 'sinon', 'expect'],
     files: [
+      'vendor/openlocationcode.js', // from https://cdnjs.cloudflare.com/ajax/libs/openlocationcode/1.0.3/openlocationcode.js
       '../node_modules/leaflet/dist/leaflet-src.js',
       '../dist/Control.Geocoder.js',
       { pattern: '**/*.spec.js' }
