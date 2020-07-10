b64c4109d3500c263a5ef00ba5a4069f04100804
Diego A. Fliess <dfliess@dfliess-hack.local>
2020-07-10 14:10:22 +0200

    fix formatting

--- src/geocoders/mapbox.js
+++ src/geocoders/mapbox.js
@@ -41,35 +41,39 @@ export var Mapbox = L.Class.extend({
     ) {
       params.proximity = params.proximity.lng + ',' + params.proximity.lat;
     }
-    getJSON(this.options.serviceUrl + encodeURIComponent(query) + '.json', params, L.bind(function(data) {
-      var results = [],
-        loc,
-        latLng,
-        latLngBounds;
-      if (data.features && data.features.length) {
-        for (var i = 0; i <= data.features.length - 1; i++) {
-          loc = data.features[i];
-          latLng = L.latLng(loc.center.reverse());
-          if (loc.bbox) {
-            latLngBounds = L.latLngBounds(
-              L.latLng(loc.bbox.slice(0, 2).reverse()),
-              L.latLng(loc.bbox.slice(2, 4).reverse())
-            );
-          } else {
-            latLngBounds = L.latLngBounds(latLng, latLng);
+    getJSON(
+      this.options.serviceUrl + encodeURIComponent(query) + '.json',
+      params,
+      L.bind(function(data) {
+        var results = [],
+          loc,
+          latLng,
+          latLngBounds;
+        if (data.features && data.features.length) {
+          for (var i = 0; i <= data.features.length - 1; i++) {
+            loc = data.features[i];
+            latLng = L.latLng(loc.center.reverse());
+            if (loc.bbox) {
+              latLngBounds = L.latLngBounds(
+                L.latLng(loc.bbox.slice(0, 2).reverse()),
+                L.latLng(loc.bbox.slice(2, 4).reverse())
+              );
+            } else {
+              latLngBounds = L.latLngBounds(latLng, latLng);
+            }
+
+            results[i] = {
+              name: loc.place_name,
+              bbox: latLngBounds,
+              center: latLng,
+              properties: this._getProperties(loc)
+            };
           }
-
-          results[i] = {
-            name: loc.place_name,
-            bbox: latLngBounds,
-            center: latLng,
-            properties: this._getProperties(loc)
-          };
         }
-      }
 
-      cb.call(context, results);
-    }, this));
+        cb.call(context, results);
+      }, this)
+    );
   },
 
   suggest: function(query, cb, context) {
