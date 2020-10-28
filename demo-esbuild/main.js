2e0553fe5f75ce4c1991bb09c5f89c9a321a6d06
Simon Legner <Simon.Legner@gmail.com>
2020-10-28 20:54:36 +0100

    demos: add esbuild bundler

---
+++ demo-esbuild/main.js
@@ -0,0 +1,14 @@
+import L from 'leaflet';
+import 'leaflet-control-geocoder';
+
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-control-geocoder/dist/Control.Geocoder.css';
+
+var map = L.map('map').setView([0, 0], 2);
+L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.Control.geocoder({
+  geocoder: L.Control.Geocoder.nominatim()
+}).addTo(map);
