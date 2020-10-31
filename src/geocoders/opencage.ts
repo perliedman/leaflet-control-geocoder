a282db1a50ad01b48b8e1f216b98abaf25f2639e
Simon Legner <Simon.Legner@gmail.com>
2020-10-31 23:16:44 +0100

    TypeScript: migrate OpenCage

--- src/geocoders/opencage.ts
+++ src/geocoders/opencage.ts
@@ -1,62 +1,30 @@
-import L from 'leaflet';
+import * as L from 'leaflet';
 import { getJSON } from '../util';
+import { GeocoderAPI, GeocodingResult } from './interfaces';
 
-export var OpenCage = L.Class.extend({
-  options: {
+export interface OpenCageOptions {
+  serviceUrl: string;
+  geocodingQueryParams?: object;
+  reverseQueryParams?: object;
+}
+
+export class OpenCage implements GeocoderAPI {
+  options: OpenCageOptions = {
     serviceUrl: 'https://api.opencagedata.com/geocode/v1/json',
     geocodingQueryParams: {},
     reverseQueryParams: {}
-  },
+  };
 
-  initialize: function(apiKey, options) {
-    L.setOptions(this, options);
-    this._accessToken = apiKey;
-  },
+  constructor(private apiKey: string, options: Partial<OpenCageOptions>) {
+    L.Util.setOptions(this, options);
+  }
 
-  geocode: function(query, cb, context) {
+  geocode(query: string, cb: (result: GeocodingResult[]) => void, context?: any): void {
     var params = {
-      key: this._accessToken,
+      key: this.apiKey,
       q: query
     };
-    params = L.extend(params, this.options.geocodingQueryParams);
-    getJSON(this.options.serviceUrl, params, function(data) {
-      var results = [],
-        latLng,
-        latLngBounds,
-        loc;
-      if (data.results && data.results.length) {
-        for (var i = 0; i < data.results.length; i++) {
-          loc = data.results[i];
-          latLng = L.latLng(loc.geometry);
-          if (loc.annotations && loc.annotations.bounds) {
-            latLngBounds = L.latLngBounds(
-              L.latLng(loc.annotations.bounds.northeast),
-              L.latLng(loc.annotations.bounds.southwest)
-            );
-          } else {
-            latLngBounds = L.latLngBounds(latLng, latLng);
-          }
-          results.push({
-            name: loc.formatted,
-            bbox: latLngBounds,
-            center: latLng
-          });
-        }
-      }
-      cb.call(context, results);
-    });
-  },
-
-  suggest: function(query, cb, context) {
-    return this.geocode(query, cb, context);
-  },
-
-  reverse: function(location, scale, cb, context) {
-    var params = {
-      key: this._accessToken,
-      q: [location.lat, location.lng].join(',')
-    };
-    params = L.extend(params, this.options.reverseQueryParams);
+    params = L.Util.extend(params, this.options.geocodingQueryParams);
     getJSON(this.options.serviceUrl, params, function(data) {
       var results = [],
         latLng,
@@ -84,8 +52,46 @@ export var OpenCage = L.Class.extend({
       cb.call(context, results);
     });
   }
-});
 
-export function opencage(apiKey, options) {
+  suggest(query: string, cb: (result: GeocodingResult[]) => void, context?: any): void {
+    return this.geocode(query, cb, context);
+  }
+
+  reverse(location: L.LatLng, scale: number, cb: (result: any) => void, context?: any): void {
+    var params = {
+      key: this.apiKey,
+      q: [location.lat, location.lng].join(',')
+    };
+    params = L.Util.extend(params, this.options.reverseQueryParams);
+    getJSON(this.options.serviceUrl, params, function(data) {
+      var results = [],
+        latLng,
+        latLngBounds,
+        loc;
+      if (data.results && data.results.length) {
+        for (var i = 0; i < data.results.length; i++) {
+          loc = data.results[i];
+          latLng = L.latLng(loc.geometry);
+          if (loc.annotations && loc.annotations.bounds) {
+            latLngBounds = L.latLngBounds(
+              L.latLng(loc.annotations.bounds.northeast),
+              L.latLng(loc.annotations.bounds.southwest)
+            );
+          } else {
+            latLngBounds = L.latLngBounds(latLng, latLng);
+          }
+          results.push({
+            name: loc.formatted,
+            bbox: latLngBounds,
+            center: latLng
+          });
+        }
+      }
+      cb.call(context, results);
+    });
+  }
+}
+
+export function opencage(apiKey: string, options: Partial<OpenCageOptions>) {
   return new OpenCage(apiKey, options);
 }
