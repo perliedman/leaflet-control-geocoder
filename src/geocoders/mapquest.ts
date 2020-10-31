04f194db8fc2cc131eb0c6f27caf1dab900a30e9
Simon Legner <Simon.Legner@gmail.com>
2020-10-31 23:12:55 +0100

    TypeScript: migrate MapQuest

--- src/geocoders/mapquest.ts
+++ src/geocoders/mapquest.ts
@@ -1,20 +1,27 @@
-import L from 'leaflet';
+import * as L from 'leaflet';
 import { getJSON } from '../util';
+import { GeocoderAPI, GeocodingResult } from './interfaces';
 
-export var MapQuest = L.Class.extend({
-  options: {
+export interface MapQuestOptions {
+  serviceUrl: string;
+}
+
+export class MapQuest implements GeocoderAPI {
+  options: MapQuestOptions = {
     serviceUrl: 'https://www.mapquestapi.com/geocoding/v1'
-  },
+  };
 
-  initialize: function(key, options) {
+  private _key: string;
+
+  constructor(key: string, options: Partial<MapQuestOptions>) {
     // MapQuest seems to provide URI encoded API keys,
     // so to avoid encoding them twice, we decode them here
     this._key = decodeURIComponent(key);
 
     L.Util.setOptions(this, options);
-  },
+  }
 
-  _formatName: function() {
+  _formatName() {
     var r = [],
       i;
     for (i = 0; i < arguments.length; i++) {
@@ -24,9 +31,9 @@ export var MapQuest = L.Class.extend({
     }
 
     return r.join(', ');
-  },
+  }
 
-  geocode: function(query, cb, context) {
+  geocode(query: string, cb: (result: GeocodingResult[]) => void, context?: any): void {
     getJSON(
       this.options.serviceUrl + '/address',
       {
@@ -35,36 +42,7 @@ export var MapQuest = L.Class.extend({
         limit: 5,
         outFormat: 'json'
       },
-      L.bind(function(data) {
-        var results = [],
-          loc,
-          latLng;
-        if (data.results && data.results[0].locations) {
-          for (var i = data.results[0].locations.length - 1; i >= 0; i--) {
-            loc = data.results[0].locations[i];
-            latLng = L.latLng(loc.latLng);
-            results[i] = {
-              name: this._formatName(loc.street, loc.adminArea4, loc.adminArea3, loc.adminArea1),
-              bbox: L.latLngBounds(latLng, latLng),
-              center: latLng
-            };
-          }
-        }
-
-        cb.call(context, results);
-      }, this)
-    );
-  },
-
-  reverse: function(location, scale, cb, context) {
-    getJSON(
-      this.options.serviceUrl + '/reverse',
-      {
-        key: this._key,
-        location: location.lat + ',' + location.lng,
-        outputFormat: 'json'
-      },
-      L.bind(function(data) {
+      L.Util.bind(function(data) {
         var results = [],
           loc,
           latLng;
@@ -84,8 +62,37 @@ export var MapQuest = L.Class.extend({
       }, this)
     );
   }
-});
 
-export function mapQuest(key, options) {
+  reverse(location: L.LatLng, scale: number, cb: (result: any) => void, context?: any): void {
+    getJSON(
+      this.options.serviceUrl + '/reverse',
+      {
+        key: this._key,
+        location: location.lat + ',' + location.lng,
+        outputFormat: 'json'
+      },
+      L.Util.bind(function(data) {
+        var results = [],
+          loc,
+          latLng;
+        if (data.results && data.results[0].locations) {
+          for (var i = data.results[0].locations.length - 1; i >= 0; i--) {
+            loc = data.results[0].locations[i];
+            latLng = L.latLng(loc.latLng);
+            results[i] = {
+              name: this._formatName(loc.street, loc.adminArea4, loc.adminArea3, loc.adminArea1),
+              bbox: L.latLngBounds(latLng, latLng),
+              center: latLng
+            };
+          }
+        }
+
+        cb.call(context, results);
+      }, this)
+    );
+  }
+}
+
+export function mapQuest(key: string, options: Partial<MapQuestOptions>) {
   return new MapQuest(key, options);
 }
