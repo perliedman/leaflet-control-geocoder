fc0eff9c49c0da8fc720fd52e94df5adea5923be
Simon Legner <Simon.Legner@gmail.com>
2020-11-28 10:36:52 +0100

    Migrate build to microbundle
    https://github.com/developit/microbundle

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+/* @preserve
+ * Leaflet Control Geocoder
+ * https://github.com/perliedman/leaflet-control-geocoder
+ *
+ * Copyright (c) 2012 sa3m (https://github.com/sa3m)
+ * Copyright (c) 2018 Per Liedman
+ * All rights reserved.
+ */
 import * as L from 'leaflet';
 import { GeocoderControl as Geocoder, geocoder } from './control';
 import * as geocoders from './geocoders/index';
