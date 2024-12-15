c55075f590912c58051732e51eb7971c428d06fa
Simon Legner <Simon.Legner@gmail.com>
2024-12-15 13:37:40 +0100

    bing: replace jsonp with AJAX

--- src/geocoders/bing.ts
+++ src/geocoders/bing.ts
@@ -1,5 +1,5 @@
 import * as L from 'leaflet';
-import { jsonp } from '../util';
+import { getJSON } from '../util';
 import {
   IGeocoder,
   GeocoderOptions,
@@ -28,38 +28,9 @@ export class Bing implements IGeocoder {
       query: query,
       key: this.options.apiKey
     });
-    jsonp(
-      this.options.serviceUrl,
-      params,
-      data => {
-        const results: GeocodingResult[] = [];
-        if (data.resourceSets.length > 0) {
-          for (let i = data.resourceSets[0].resources.length - 1; i >= 0; i--) {
-            const resource = data.resourceSets[0].resources[i],
-              bbox = resource.bbox;
-            results[i] = {
-              name: resource.name,
-              bbox: L.latLngBounds([bbox[0], bbox[1]], [bbox[2], bbox[3]]),
-              center: L.latLng(resource.point.coordinates)
-            };
-          }
-        }
-        cb.call(context, results);
-      },
-      this,
-      'jsonp'
-    );
-  }
-
-  reverse(location: L.LatLngLiteral, scale: number, cb: GeocodingCallback, context?: any): void {
-    const params = reverseParams(this.options, {
-      key: this.options.apiKey
-    });
-    jsonp(
-      this.options.serviceUrl + location.lat + ',' + location.lng,
-      params,
-      data => {
-        const results: GeocodingResult[] = [];
+    getJSON(this.options.serviceUrl, params, data => {
+      const results: GeocodingResult[] = [];
+      if (data.resourceSets.length > 0) {
         for (let i = data.resourceSets[0].resources.length - 1; i >= 0; i--) {
           const resource = data.resourceSets[0].resources[i],
             bbox = resource.bbox;
@@ -69,11 +40,28 @@ export class Bing implements IGeocoder {
             center: L.latLng(resource.point.coordinates)
           };
         }
-        cb.call(context, results);
-      },
-      this,
-      'jsonp'
-    );
+      }
+      cb.call(context, results);
+    });
+  }
+
+  reverse(location: L.LatLngLiteral, scale: number, cb: GeocodingCallback, context?: any): void {
+    const params = reverseParams(this.options, {
+      key: this.options.apiKey
+    });
+    getJSON(this.options.serviceUrl + location.lat + ',' + location.lng, params, data => {
+      const results: GeocodingResult[] = [];
+      for (let i = data.resourceSets[0].resources.length - 1; i >= 0; i--) {
+        const resource = data.resourceSets[0].resources[i],
+          bbox = resource.bbox;
+        results[i] = {
+          name: resource.name,
+          bbox: L.latLngBounds([bbox[0], bbox[1]], [bbox[2], bbox[3]]),
+          center: L.latLng(resource.point.coordinates)
+        };
+      }
+      cb.call(context, results);
+    });
   }
 }
 
