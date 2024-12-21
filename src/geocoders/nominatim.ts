04031ac0d782d6f5e69fba0d15bfadbb2910efbb
Simon Legner <Simon.Legner@gmail.com>
2024-12-21 10:37:07 +0100

    nominatim: use shorthand property for htmlTemplate

--- src/geocoders/nominatim.ts
+++ src/geocoders/nominatim.ts
@@ -59,7 +59,7 @@ export interface NominatimOptions extends GeocoderOptions {
 export class Nominatim implements IGeocoder {
   options: NominatimOptions = {
     serviceUrl: 'https://nominatim.openstreetmap.org/',
-    htmlTemplate: function (r: NominatimResult) {
+    htmlTemplate(r: NominatimResult) {
       const address = r.address;
       let className: string;
       const parts: string[] = [];
