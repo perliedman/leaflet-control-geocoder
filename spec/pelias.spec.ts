a0c2f4a295f73e03eae0274e537d17cf35c43d92
Peter Johnson <insomnia@rcpt.at>
2025-06-02 15:53:16 +0200

    pelias: update type defs

--- spec/pelias.spec.ts
+++ spec/pelias.spec.ts
@@ -13,8 +13,17 @@ describe('L.Control.Geocoder.Openrouteservice', () => {
         geocoding: {
           version: '0.2',
           attribution: 'openrouteservice.org | OpenStreetMap contributors | Geocoding by Pelias',
-          query: {},
-          warnings: ["performance optimization: excluding 'address' layer"],
+          query: {
+            size: 10,
+            lang:  {
+              name: 'English',
+              iso6391: 'en',
+              iso6393: 'eng',
+              via: 'header',
+              defaulted: false
+            }
+          },
+          warnings: [`performance optimization: excluding 'address' layer`],
           engine: { name: 'Pelias', author: 'Mapzen', version: '1.0' }
         },
         type: 'FeatureCollection',
@@ -24,22 +33,45 @@ describe('L.Control.Geocoder.Openrouteservice', () => {
             geometry: { type: 'Point', coordinates: [11.407851, 47.272308] },
             properties: {
               id: '101748061',
+              gid: 'whosonfirst:locality:101748061',
               layer: 'locality',
+              source: 'whosonfirst',
               source_id: '101748061',
+              country_code: 'AT',
               name: 'Innsbruck',
-              confidence: 1,
-              match_type: 'exact',
+              distance: 739.011,
               accuracy: 'centroid',
               country: 'Austria',
+              country_gid: 'whosonfirst:country:85632785',
               country_a: 'AUT',
-              region: 'Tirol',
+              region: 'Tyrol',
+              region_gid: 'whosonfirst:region:85681661',
               region_a: 'TR',
               county: 'Innsbruck',
-              county_a: 'IN',
+              county_gid: 'whosonfirst:county:102049723',
               localadmin: 'Innsbruck',
+              localadmin_gid: 'whosonfirst:localadmin:1108837687',
               locality: 'Innsbruck',
+              locality_gid: 'whosonfirst:locality:101748061',
               continent: 'Europe',
-              label: 'Innsbruck, Austria'
+              continent_gid: 'whosonfirst:continent:102191581',
+              label: 'Innsbruck, TR, Austria',
+              category: [
+                'admin:admin2',
+                'admin'
+              ],
+              addendum: {
+                concordances: {
+                  'dbp:id': 'Innsbruck',
+                  'fb:id': 'en.innsbruck',
+                  'gn:id': 2775220,
+                  'gp:id': 550763,
+                  'nyt:id': 'N21126484242539583751',
+                  'qs_pg:id': 274233,
+                  'wd:id': 'Q1735',
+                  'wk:page': 'Innsbruck'
+                }
+              }
             },
             bbox: [11.3218091258, 47.2470573997, 11.452584553, 47.29398]
           }
@@ -50,7 +82,7 @@ describe('L.Control.Geocoder.Openrouteservice', () => {
     );
 
     const feature = result[0];
-    expect(feature.name).toBe('Innsbruck, Austria');
+    expect(feature.name).toBe('Innsbruck, TR, Austria');
     expect(feature.center).toStrictEqual({ lat: 47.272308, lng: 11.407851 });
     expect(feature.bbox).toStrictEqual({
       _southWest: { lat: 47.2470573997, lng: 11.3218091258 },
--- src/geocoders/pelias.ts
+++ src/geocoders/pelias.ts
@@ -49,9 +49,10 @@ export class Pelias implements IGeocoder {
       const c = f.geometry.coordinates;
       const center = new L.LatLng(c[1], c[0]);
 
-      const bbox = (Array.isArray(f.bbox) && f.bbox.length === 4)
-        ? new L.LatLngBounds([f.bbox[1], f.bbox[0]], [f.bbox[3], f.bbox[2]])
-        : new L.LatLngBounds(center, center);
+      const bbox =
+        Array.isArray(f.bbox) && f.bbox.length === 4
+          ? new L.LatLngBounds([f.bbox[1], f.bbox[0]], [f.bbox[3], f.bbox[2]])
+          : new L.LatLngBounds(center, center);
 
       return {
         name: f.properties!.label,
@@ -116,39 +117,130 @@ export type PeliasResponse = GeoJSON.FeatureCollection<GeoJSON.Geometry, Propert
   geocoding: Geocoding;
 };
 
-interface Properties {
-  id:         string;
-  layer:      string;
-  source_id:  string;
-  name:       string;
-  confidence: number;
-  match_type: string;
-  accuracy:   string;
-  country:    string;
-  country_a:  string;
-  region:     string;
-  region_a:   string;
-  county:     string;
-  county_a:   string;
-  localadmin: string;
-  locality:   string;
-  continent:  string;
-  label:      string;
+interface Identity {
+  id: string;
+  gid: string;
+  layer: string;
+  source: string;
+  source_id: string;
 }
 
+interface Labels {
+  name: string;
+  label: string;
+  category?: string[];
+}
+
+interface Hierarchy {
+  country_code?: string;
+
+  ocean?: string;
+  ocean_gid?: string;
+  ocean_a?: string;
+
+  marinearea?: string;
+  marinearea_gid?: string;
+  marinearea_a?: string;
+
+  continent?: string;
+  continent_gid?: string;
+  continent_a?: string;
+
+  empire?: string;
+  empire_gid?: string;
+  empire_a?: string;
+
+  country?: string;
+  country_gid?: string;
+  country_a?: string;
+
+  dependency?: string;
+  dependency_gid?: string;
+  dependency_a?: string;
+
+  macroregion?: string;
+  macroregion_gid?: string;
+  macroregion_a?: string;
+
+  region?: string;
+  region_gid?: string;
+  region_a?: string;
+
+  macrocounty?: string;
+  macrocounty_gid?: string;
+  macrocounty_a?: string;
+
+  county?: string;
+  county_gid?: string;
+  county_a?: string;
+
+  localadmin?: string;
+  localadmin_gid?: string;
+  localadmin_a?: string;
+
+  locality?: string;
+  locality_gid?: string;
+  locality_a?: string;
+
+  borough?: string;
+  borough_gid?: string;
+  borough_a?: string;
+
+  neighbourhood?: string;
+  neighbourhood_gid?: string;
+  neighbourhood_a?: string;
+
+  postalcode?: string;
+  postalcode_gid?: string;
+  postalcode_a?: string;
+}
+
+interface Address {
+  unit?: string;
+  housenumber?: string;
+  street?: string;
+  postalcode?: string;
+}
+
+interface Scoring {
+  accuracy: string;
+  confidence?: number;
+  distance?: number;
+  match_type?: string;
+}
+
+interface Addendum {
+  addendum?: Record<string, Object>;
+}
+
+interface Properties extends Identity, Labels, Scoring, Address, Hierarchy, Addendum {}
+
 interface Geocoding {
-  version:     string;
+  version: string;
   attribution: string;
-  query:       Query;
-  warnings:    string[];
-  engine:      Engine;
+  query: Query;
+  warnings: string[];
+  engine: Engine;
 }
 
 interface Engine {
-  name:    string;
-  author:  string;
+  name: string;
+  author: string;
   version: string;
 }
 
 interface Query {
+  size: number;
+  lang: {
+    name: string;
+    iso6391: string;
+    iso6393: string;
+    via: string;
+    defaulted: boolean
+  }
+  text?: string;
+  parser?: string;
+  parsed_text?: Record<string, string>;
+  sources?: string[];
+  layers?: string[];
 }
